2ab66f638065311ff3095c963c7409ae304e317d
Rafael Nieto <rafael.nieto@passportscan.net>
2023-05-22 13:00:48 +0200

    ADD achevement entity

---
+++ src/Repository/AchievementRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Achievement;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Achievement>
+ *
+ * @method Achievement|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Achievement|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Achievement[]    findAll()
+ * @method Achievement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AchievementRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Achievement::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Achievement $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Achievement $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    // /**
+    //  * @return Achievement[] Returns an array of Achievement objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Achievement
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
